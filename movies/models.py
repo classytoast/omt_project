96c3ffffdb6ba0671aacb29b0cc0d896d2ca8c1f
Dmitriy <lion95-10@mail.ru>
2024-05-14 23:06:10 +0300

    added tables to models and showed them in admin

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,88 @@
+import uuid
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class TimeStampedMixin(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class UUIDMixin(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class Genre(UUIDMixin, TimeStampedMixin):
+    name = models.CharField('name', max_length=255)
+    description = models.TextField('description', blank=True)
+
+    class Meta:
+        # Таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
+        db_table = "content\".\"genre"
+        # Следующие два поля отвечают за название модели в интерфейсе
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class FilmWork(UUIDMixin, TimeStampedMixin):
+
+    class TypeChoices(models.TextChoices):
+        MOVIE = 'movie', 'Movie'
+        TV_SHOW = 'tv_show', 'TV Show'
+
+    title = models.CharField('name', max_length=255)
+    description = models.TextField('description', blank=True)
+    creation_date = models.DateField()
+    rating = models.FloatField('rating', blank=True,
+                               validators=[MinValueValidator(0),
+                                           MaxValueValidator(100)])
+    type = models.CharField(max_length=20, choices=TypeChoices.choices, null=False)
+    genres = models.ManyToManyField(Genre, through='GenreFilmwork')
+
+    class Meta:
+        db_table = "content\".\"film_work"
+        verbose_name = 'Кинопроизведение'
+        verbose_name_plural = 'Кинопроизведения'
+
+    def __str__(self):
+        return self.title
+
+
+class GenreFilmWork(UUIDMixin):
+    film_work = models.ForeignKey('FilmWork', on_delete=models.CASCADE)
+    genre = models.ForeignKey('Genre', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "content\".\"genre_film_work"
+
+
+class Person(UUIDMixin, TimeStampedMixin):
+    full_name = models.CharField('full name', max_length=255)
+
+    class Meta:
+        db_table = "content\".\"person"
+        verbose_name = 'Персона'
+        verbose_name_plural = 'Персоны'
+
+    def __str__(self):
+        return self.full_name
+
+
+class PersonFilmWork(UUIDMixin):
+    film_work = models.ForeignKey('FilmWork', on_delete=models.CASCADE)
+    person = models.ForeignKey('Person', on_delete=models.CASCADE)
+    role = models.TextField('role', null=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "content\".\"person_film_work"
