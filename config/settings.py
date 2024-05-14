c72e230a7b9e906da063d1d626d3e7af4604741b
Dmitriy <lion95-10@mail.ru>
2024-05-14 21:52:53 +0300

    updated gitignore and done first migrations

--- config/settings.py
+++ config/settings.py
@@ -16,10 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 ROOT_URLCONF = 'config.urls'
 
